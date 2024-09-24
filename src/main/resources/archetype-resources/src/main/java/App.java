05aacd24a1f965ed5fe4c6b41dfa95f2ee7ad867
Ranjith B <am.ranju007@gmail.com>
2024-09-24 09:52:25 +0530

    initial commit of git_practice

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.jthread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
