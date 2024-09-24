76a11692d0891a46fcf6d00d4eb298e1f0041939
Ranjith B <am.ranju007@gmail.com>
2024-09-24 09:58:51 +0530

    added Main Class

---
+++ src/main/java/com/verteil/Main.java
@@ -0,0 +1,11 @@
+package com.verteil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("git practice");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
